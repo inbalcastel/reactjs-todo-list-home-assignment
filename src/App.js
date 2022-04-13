6432fad9238eb5b3da513ce1760144468b300f9d
inbalcastel <97899250+inbalcastel@users.noreply.github.com>
2022-04-13 18:03:40 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
+
+import MyList from "./components/MyList";
+import ToDoProvider from "./Context/ToDoState";
+
 export default function App() {
   return (
     <div>
       <h1>My List</h1>
+        <ToDoProvider>
+            <MyList />
+        </ToDoProvider>
     </div>
   );
 }
